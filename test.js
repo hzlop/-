268375983340a6633ea79bdd56e006dab60fbd8f
hzlop <141869802+hzlop@users.noreply.github.com>
2026-03-06 15:47:55 +0800

    Add new userscript template in test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @updataURL    https://raw.githubusercontent.com/hzlop/-/main/test.js
+// @downloadURL  https://raw.githubusercontent.com/hzlop/-/main/test.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('hello hzlop');
+    // Your code here...
+})();
